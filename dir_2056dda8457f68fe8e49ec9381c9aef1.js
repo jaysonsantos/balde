648b79207300ac73468931ecd83d2b1ab8c7d4dc
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-02 03:00:24 -0200

    Updated docs.

--- dir_2056dda8457f68fe8e49ec9381c9aef1.js
+++
@@ -1,4 +0,0 @@
-var dir_2056dda8457f68fe8e49ec9381c9aef1 =
-[
-    [ "foo.h", "foo_8h_source.html", null ]
-];
--- files.js
+++ files.js
@@ -1,5 +1,4 @@
 var files =
 [
-    [ "examples", "dir_5ccd7d5da5aaf591ea99da588ea45c75.html", "dir_5ccd7d5da5aaf591ea99da588ea45c75" ],
     [ "include", "dir_0376fae09af503611eb3e1812ba4fe9e.html", "dir_0376fae09af503611eb3e1812ba4fe9e" ]
 ];

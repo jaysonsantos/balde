a0537ed61ac59419fd5b2bb6a4393f6cc70e265e
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-11 20:47:20 -0200

    Updated docs.

--- app_8h.js
+++ app_8h.js
@@ -4,6 +4,8 @@ var app_8h =
     [ "balde_view_func_t", "app_8h.html#aa84a1f05581a47984022541b0f11c647", null ],
     [ "balde_app_add_url_rule", "app_8h.html#a859614273e71fe0e1b24926b4818f7aa", null ],
     [ "balde_app_free", "app_8h.html#a7f70dda0b646abb68686ff3fcaf605f2", null ],
+    [ "balde_app_get_config", "app_8h.html#a02dcf6c64e2c484849473231eee927a4", null ],
     [ "balde_app_init", "app_8h.html#ad9173f43f1febcb85b67b9ffc03b9e78", null ],
-    [ "balde_app_run", "app_8h.html#a7ee7602303231372ce0a033c652e34dc", null ]
+    [ "balde_app_run", "app_8h.html#a7ee7602303231372ce0a033c652e34dc", null ],
+    [ "balde_app_set_config", "app_8h.html#a1f119639d265a8ad81d96a9fabdd4a32", null ]
 ];

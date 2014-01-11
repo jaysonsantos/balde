c1b79bfed06877a72935d3e107fa561962573a5b
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2014-01-11 21:19:59 -0200

    Updated docs.

--- wrappers_8h.js
+++ wrappers_8h.js
@@ -3,12 +3,12 @@ var wrappers_8h =
     [ "balde_request_t", "structbalde__request__t.html", "structbalde__request__t" ],
     [ "balde_response_t", "structbalde__response__t.html", "structbalde__response__t" ],
     [ "balde_make_response", "wrappers_8h.html#af3c56af8e1316c0894bbcb8056c7d5a8", null ],
-    [ "balde_request_get_arg", "wrappers_8h.html#acb973926424c1e4d3068f1ce4f4d8d51", null ],
-    [ "balde_request_get_form", "wrappers_8h.html#ad2f3ad91f52decff41d8114e28a14c91", null ],
-    [ "balde_request_get_header", "wrappers_8h.html#a3e6b92b8e62961ebc7873499953c273a", null ],
-    [ "balde_request_get_view_arg", "wrappers_8h.html#a17b15b0f065ba14f1a6a757ef450750e", null ],
+    [ "balde_request_get_arg", "wrappers_8h.html#a1e964c6c06a3500ba283310f6b26c6c6", null ],
+    [ "balde_request_get_form", "wrappers_8h.html#ad0970a629527b13d7a0a743d2818cbad", null ],
+    [ "balde_request_get_header", "wrappers_8h.html#ae2d32b038d4f196376fc942e2fce900e", null ],
+    [ "balde_request_get_view_arg", "wrappers_8h.html#a751237022c60a7243228f381dff51619", null ],
     [ "balde_response_append_body", "wrappers_8h.html#aa309931be43c2190a10482b2e6cdebb0", null ],
-    [ "balde_response_get_tmpl_var", "wrappers_8h.html#a1ad0207d4c65a3cfbbf53b2944941584", null ],
-    [ "balde_response_set_header", "wrappers_8h.html#a08594f712094af30909013827ac26900", null ],
-    [ "balde_response_set_tmpl_var", "wrappers_8h.html#aec46014e8851c929d860148102ab85e0", null ]
+    [ "balde_response_get_tmpl_var", "wrappers_8h.html#a1d39feefcfed8ad0d711a2b84ef9d967", null ],
+    [ "balde_response_set_header", "wrappers_8h.html#ad5fb729beaaea090ca9fd7e6e35c188c", null ],
+    [ "balde_response_set_tmpl_var", "wrappers_8h.html#aa5c197fcdbf5652ea4d459bb7506f300", null ]
 ];
